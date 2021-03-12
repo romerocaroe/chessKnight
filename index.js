7c5f1831a4472a2e4685d17163ca1488fbea60fd
Carolina Romero <carolina.romero@sondeos.com.ar>
2021-03-12 18:06:50 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const challengeFunction = function(cell){
-    /* YOUR CODE HERE */
-    let result
-
+    let result = 0;
+    let strgLetters = ['A','B','C','D','E','F','G','H']
+    let strgNumbers = [1,2,3,4,5,6,7,8]
+    let letter = cell.split('')[0];
+    let number = cell.split('')[1];
+    let positionLetter = strgLetters.indexOf(letter);
+    let positionNumber = parseInt(number);
+    validatePosition(positionLetter+1,positionNumber+2) ? result++ : result
+    validatePosition(positionLetter+1,positionNumber-2) ? result++ : result
+    validatePosition(positionLetter-1,positionNumber+2) ? result++ : result
+    validatePosition(positionLetter-1,positionNumber-2) ? result++ : result
+    validatePosition(positionLetter+2,positionNumber+1) ? result++ : result
+    validatePosition(positionLetter+2,positionNumber-1) ? result++ : result
+    validatePosition(positionLetter-2,positionNumber+1) ? result++ : result
+    validatePosition(positionLetter-2,positionNumber-1) ? result++ : result
     return result
+}
+
+const validatePosition = function(letter,number){
+    console.log(letter,number)
+    return (letter >= 0 && letter <= 7) && (number >= 1 && number <= 8)
 }
 
 module.exports = challengeFunction
